e82de0d534006c4762b0265d66b6676a5b8d1925
Fernando Chavez <fernando.chavez@gdpteam.com>
2025-03-04 00:40:34 -0500

    feat: fixed security authorization filters and CSRF

--- src/main/java/pj/gob/pe/gateway/config/ProjectSecurityConfig.java
+++ src/main/java/pj/gob/pe/gateway/config/ProjectSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import pj.gob.pe.gateway.exceptionhandling.CustomAccessDeniedHandler;
 import pj.gob.pe.gateway.filter.CsrfCookieFilter;
 
@@ -21,33 +22,47 @@ import java.util.Collections;
 @Configuration
 public class ProjectSecurityConfig {
 
+    // Configuración de CORS
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200")); // Origen permitido
+        config.setAllowedMethods(Collections.singletonList("*")); // Métodos permitidos
+        config.setAllowCredentials(true); // Permitir credenciales
+        config.setAllowedHeaders(Collections.singletonList("*")); // Cabeceras permitidas
+        config.setExposedHeaders(Arrays.asList("Authorization")); // Cabeceras expuestas
+        config.setMaxAge(3600L); // Tiempo máximo de caché para CORS
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // Aplicar a todas las rutas
+        return source;
+    }
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
-        CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
+        //CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
         http.sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .cors(corsConfig -> corsConfig.configurationSource(new CorsConfigurationSource() {
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                        CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-                        config.setAllowedMethods(Collections.singletonList("*"));
-                        config.setAllowCredentials(true);
-                        config.setAllowedHeaders(Collections.singletonList("*"));
-                        config.setExposedHeaders(Arrays.asList("Authorization"));
-                        config.setMaxAge(3600L);
-                        return config;
-                    }
-                }))
-                .csrf(csrfConfig -> csrfConfig.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
-                        .ignoringRequestMatchers("/contact", "/register")
-                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
-                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
-                .requiresChannel(rcc -> rcc.anyRequest().requiresInsecure()) // Only HTTP
+                // Configuración de CORS
+                .cors(corsConfig -> corsConfig.configurationSource(corsConfigurationSource()))
+
+                .csrf(csrf -> csrf.disable())
+                //.csrf(csrfConfig -> csrfConfig.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+//                .ignoringRequestMatchers("/contact", "/register")
+//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+//                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+//                .requiresChannel(rcc -> rcc.anyRequest().requiresInsecure()) // Only HTTP
                 .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/api/security/v1/auth/login").permitAll()
+                        .requestMatchers("/api/security/v1/auth/logout").permitAll()
                         .requestMatchers("/api/security/**").authenticated()
                         .requestMatchers("/v1/test/*").authenticated());
+        http.oauth2ResourceServer(rsc -> rsc.jwt(jwtConfigurer ->
+                jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+        http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
+        return http.build();
+    }
+}
 
 //                        .requestMatchers("/myAccount").hasRole("USER")
 //                        .requestMatchers("/myBalance").hasAnyRole("USER", "ADMIN")
@@ -55,11 +70,5 @@ public class ProjectSecurityConfig {
 //                        .requestMatchers("/myCards").hasRole("USER")
 //                        .requestMatchers("/user").authenticated()
 //                        .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
-        http.oauth2ResourceServer(rsc -> rsc.jwt(jwtConfigurer ->
-                jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter)));
-        /*http.oauth2ResourceServer(rsc -> rsc.opaqueToken(otc -> otc.authenticationConverter(new KeycloakOpaqueRoleConverter())
+/*http.oauth2ResourceServer(rsc -> rsc.opaqueToken(otc -> otc.authenticationConverter(new KeycloakOpaqueRoleConverter())
                 .introspectionUri(this.introspectionUri).introspectionClientCredentials(this.clientId,this.clientSecret)));*/
-        http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
-        return http.build();
-    }
-}
